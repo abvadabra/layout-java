f427a95955f675d101489b216b945877b278948e
Danil Dubrovksy <forwot163@gmail.com>
2022-09-11 21:27:10 +0300

    Changed scope of jetbrains annotation dependency from implementation to compileOnly to avoid forceful inclusion in users' projects

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains:annotations:23.0.0")
+    compileOnly("org.jetbrains:annotations:23.0.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
